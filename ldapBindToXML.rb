e91e097a716f4c3a0ca2f8cf8a1e7142f9630e15
Loïc Chanel <loic.chanel@worldline.com>
2015-06-24 10:09:23 -0400

    Script mapping LDAP users to their group for Knox complete

--- ldapBindToXML.rb
+++ ldapBindToXML.rb
@@ -4,33 +4,35 @@ require 'rubygems'
 require 'net/ldap'
 require 'xmlsimple'
 
-if ARGV.size = 0
+if ARGV.empty?
   puts "No topology file given in argument. Using /etc/knox/conf/topologies/default.xml"
   topoFile = "/etc/knox/conf/topologies/default.xml"
 else
-  topoFile = ARGV[1]
+  topoFile = ARGV[0]
 end
 
-ldap = Net::LDAP.new :host => 'ypliam01b.intcs.meshcore.net', :port => 636, :auth => {:method => :anonymous }
+ldap = Net::LDAP.new :host => 'ypliam01b.intcs.meshcore.net', :port => 389, :auth => {:method => :anonymous }
 
 base = 'dc=prd,dc=mutu,dc=fr'
-filter = Net::LDAP::Filter.eq('objectclass', 'person')
+filter = Net::LDAP::Filter.eq("objectclass", "person") &  Net::LDAP::Filter.pres("memberOf")
 
 csvGroup = ""
 ldap.search(:base => base, :filter => filter) do |object|
-  object.memberOf.each do |group|
-    csvGroup << object.cn << "=" << group << ";"
+  object[:memberOf].to_a.each do |group|
+    csvGroup << object[:cn][0] << "=" << group.split("=")[1].split(',')[0] << ";"
   end
 end
 
-puts csvGroup
-
+groupMapping = ''
 topology = XmlSimple.xml_in(topoFile)
-topology['topology']['gateway']['provider'].each do |provider|
-  if provider['role']=="identity-assertion"
+topology['gateway'][0]['provider'].each do |provider|
+  if provider['role'][0]=="identity-assertion"
     provider['param'].each do |params|
-      if params['name']=="group.principal.mapping"
-        groupMapping = params['value']
+      if params['name'][0]=="group.principal.mapping"
+        groupMapping = params['value'][0]
+        if csvGroup != groupMapping
+          params['value'][0] = csvGroup
+        end
         break
       else
         next
@@ -42,5 +44,11 @@ topology['topology']['gateway']['provider'].each do |provider|
 end
 
 if csvGroup != groupMapping
-  `sed -i "s/#{groupMapping}/#{csvGroup}/" #{topoFile}`
+  File.open(topoFile, 'w') do |file| 
+    file.write( XmlSimple.xml_out(topology))
+    file.close
+  end
+  `sed -i s/opt>/topology>/ #{topoFile}`
+  `find /usr/hdp -iname 'gateway.sh' -exec {} stop \\;`
+  `find /usr/hdp -iname 'gateway.sh' -exec {} start \\;`
 end
