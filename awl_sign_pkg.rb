226d0a83f96494441c5aa4c0de8b5e0da8d4794e
Loïc C. Chanel <loic.chanel@telecomnancy.net>
2016-03-11 11:58:03 -0500

    Debug over re-signing script

--- awl_sign_pkg.rb
+++ awl_sign_pkg.rb
@@ -1,33 +1,29 @@
 require 'json'
-
-# Aller lire le fichier de conf globale
-CONF_FILE = "/etc/pds-packaging/config.json"
-raise "Configuration file #{CONF_FILE} not found" unless File.readable? CONF_FILE
-CONF = JSON.parse(IO.read(CONF_FILE), :symbolize_names => true)
+require 'tempfile'
 
 # Aller lire le fichier qui liste les repo à resigner
-repolist = File.read('repo_to_resign')
-repolist.each do |repo|
+repolist = File.read('repo_to_resign.conf').split "\n"
+repolist.each do |line|
   next if line.include? "#"
   next if line.empty?
 
-  Dir["/DATA/repositories/#{repo}/**/*.rpm"].each do |rpm|
+  Dir["/DATA/repositories/#{line}/**/*.rpm"].each do |rpm|
     begin
       raise "#{rpm} not found" unless File.exist? rpm
-      log "Signing #{rpm}"
+      puts "Signing #{rpm}"
       Tempfile.open('sign-rpm-rpmmacros') do |f|
         begin
           f.write "
 %_tmppath   /tmp
 %_signature gpg
-%_gpg_path  #{CONF[:dir][:keystore]}
-%_gpg_name  awl@#{CONF[:gpg_mail_domain]}
+%_gpg_path  /DATA/GPG-keys/private
+%_gpg_name  awl@repositories.priv.atos.fr
 
-%__gpg_check_password_cmd #{CONF[:bin][:gpg]} --batch --no-verbose -u \"%{_gpg_name}\" -so -
-%__gpg_sign_cmd #{CONF[:bin][:gpg]} --batch --no-verbose --no-armor --no-secmem-warning -u \"%{_gpg_name}\" -sbo %{__signature_filename} %{__plaintext_filename}
+%__gpg_check_password_cmd /usr/bin/gpg --batch --no-verbose -u \"%{_gpg_name}\" -so -
+%__gpg_sign_cmd /usr/bin/gpg --batch --no-verbose --no-armor --no-secmem-warning -u \"%{_gpg_name}\" -sbo %{__signature_filename} %{__plaintext_filename}
 "
           f.close
-          output = `#{CONF[:bin][:expect]} -c "spawn #{CONF[:bin][:rpm]} -v --macros=#{f.path} --addsign #{rpm}" -c 'expect "pass phrase"' -c 'send -- "\r"' -c "expect eof" -c 'catch wait result' -c 'exit [lindex $result 3]' 2>&1`
+          output = `/usr/bin/expect -c "spawn /bin/rpm -v --macros=#{f.path} --addsign #{rpm}" -c 'expect "pass phrase"' -c 'send -- "\r"' -c "expect eof" -c 'catch wait result' -c 'exit [lindex $result 3]' 2>&1`
           raise "rpm returns #{$?}: #{output}" unless $? == 0
         ensure
           f.close
