55ce4cd51f6aa974dcc3665154920f7276aeebd4
Loïc C. Chanel <loic.chanel@telecomnancy.net>
2016-03-21 18:22:16 -0400

    Minor modification

--- mail-safebox/docs/mail-checker.rb
+++ mail-safebox/docs/mail-checker.rb
@@ -8,12 +8,12 @@ params = YAML.load_file('configuration.yaml')
 yesterday = Date.today.prev_day
 
 Gmail.new(params['mailbox']['user_name'], params['mailbox']['password']) do |gmail|
-  return File.write('safebox_remaining_days', 30) if gmail.label("[Gmail]/Messages envoy&AOk-s").count(:after => Date.parse(yesterday.strftime('%Y-%m-%d'))) == 0
+  return File.write('safebox_remaining_days', params['safebox_max_days']) if gmail.label("[Gmail]/Messages envoy&AOk-s").count(:after => Date.parse(yesterday.strftime('%Y-%m-%d'))) == 0
 end
 
 Gmail.new(params['safebox']['user_name'], params['safebox']['password']) do |gmail|
   gmail.deliver do
-    to "loic.chanel@gmail.com"
+    to params['user_personnal_mail']
     subject 'Safebox warning'
     text_part do 
       body "Please answer this eMail within #{remaining_days} days or the safebox will be emptied"
